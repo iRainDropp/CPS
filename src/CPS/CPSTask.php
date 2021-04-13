3b1e1d7d542a8c95ad1b379ba6a50bb0fd3b467a
Austin Desrochers <mcpedudes12@gmail.com>
2021-04-13 16:38:53 -0700

    Update CPSTask.php
    Changed the str_replace for replacing "20" with "20+" in the CPS Alerts to instead check the CPS Limit and if their amount of CPS is that or higher, if it is, it will say "{CPS LIMIT}+" instead of always saying 20+, even if you were to change the CPS Limit through the config.

--- src/CPS/CPSTask.php
+++ src/CPS/CPSTask.php
@@ -47,7 +47,7 @@ class CPSTask extends Task
                         if($player->hasPermission("cps.alerts")){
                             $message = str_replace("{playername}", $playerName, $this->plugin->cpsAlerts);
                             $message = str_replace("{cps}", $this->plugin->realCPS[$playerName], $message);
-                            $message = str_replace("20", "20+", $message);
+                            $message = str_replace($this->plugin->CPSLimit, $this->plugin->CPSLimit . "+", $message);
                             $player->sendMessage($message);
                         }
                         $this->delay = time() + $this->plugin->alertCooldown;
