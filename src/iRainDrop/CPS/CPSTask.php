dbc54f2175fd0e136eefb8e9ce777d7fcb1fcf7f
Austin Desrochers <mcpedudes12@gmail.com>
2021-04-14 18:10:46 -0700

    Add files via upload

---
+++ src/iRainDrop/CPS/CPSTask.php
@@ -0,0 +1,59 @@
+<?php
+
+
+namespace iRainDrop\CPS;
+
+use pocketmine\Player;
+use pocketmine\scheduler\Task;
+use pocketmine\utils\TextFormat;
+
+class CPSTask extends Task
+{
+    private $plugin;
+    private $delay = 0;
+
+    public function __construct(Main $plugin)
+    {
+        $this->plugin = $plugin;
+
+    }
+
+    public function onRun(int $currentTick)
+    {
+        $this->updateHud();
+        foreach($this->plugin->tip as $playerName => $cps){
+            if($cps >= 1)
+            {
+                $this->plugin->tip[$playerName] = 0;
+            }
+            if($this->plugin->realCPS[$playerName] >= 1)
+            {
+                $this->plugin->realCPS[$playerName] = 0;
+            }
+        }
+    }
+
+    public function updateHud()
+    {
+        foreach($this->plugin->tip as $playerName => $cps){
+            if($this->plugin->Enabled[$playerName] === true)
+            {
+                $p = $this->plugin->getServer()->getPlayer($playerName);
+                $p->sendTip($this->plugin->CPSMessage . $cps);
+            }
+            if($this->delay <time()){
+                if($this->plugin->realCPS[$playerName] >= $this->plugin->cpsAmount){
+                    foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
+                        if($player->hasPermission("cps.alerts")){
+                            $message = str_replace("{playername}", $playerName, $this->plugin->cpsAlerts);
+                            $message = str_replace("{cps}", $this->plugin->realCPS[$playerName], $message);
+                            $message = str_replace($this->plugin->CPSLimit, $this->plugin->CPSLimit . "+", $message);
+                            $player->sendMessage($message);
+                        }
+                        $this->delay = time() + $this->plugin->alertCooldown;
+                    }
+                }
+            }
+        }
+    }
+}
