8c98d55e49aca12d5dd293e7c197f44aaa28213e
Austin Desrochers <mcpedudes12@gmail.com>
2022-03-20 13:39:16 -0700

    Add files via upload

---
+++ src/iRainDrop/CPS/Main.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace iRainDrop\CPS;
+
+use iRainDrop\CPS\Commands\CPSCommand;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase{
+
+    public static $cpsEnabled = [];
+    public static $config;
+    public static $lang;
+
+    function onEnable(): void{
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener, $this);
+        $this->getServer()->getCommandMap()->register("CPS", new CPSCommand ("cps", "Enable or disable the CPS popup."));
+        $this->saveresource("config.yml");
+        $this->saveresource("lang.yml");
+        self::$config = new Config($this->getDataFolder() . "config.yml");
+        self::$lang = new Config($this->getDataFolder() . "lang.yml");
+    }
+}
